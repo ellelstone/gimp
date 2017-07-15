5d931d1dd43d1d4c7126d61a39fa3975bebd61c6
Elle Stone <ellestone@ninedegreesbelow.com>
2017-07-15 06:03:34 -0400

    Bring gimptoolinfo.c in line with upstream.

--- app/core/gimptoolinfo.c
+++ app/core/gimptoolinfo.c
@@ -194,11 +194,12 @@ static gchar *
 gimp_tool_info_get_description (GimpViewable  *viewable,
                                 gchar        **tooltip)
 {
-//  GimpToolInfo *tool_info = GIMP_TOOL_INFO (viewable);
+  GimpToolInfo *tool_info = GIMP_TOOL_INFO (viewable);
 
-//  *tooltip = g_strdup (tool_info->tooltip);
+  if (tooltip)
+    *tooltip = g_strdup (tool_info->tooltip);
 
-  return NULL;// g_strdup (tool_info->label);
+  return g_strdup (tool_info->label);
 }
 
 static gboolean
