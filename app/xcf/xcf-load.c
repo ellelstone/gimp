3755b84f55f44ea31f0146a118854574e0e23acb
Michael Natterer <mitch@gimp.org>
2017-09-16 19:03:20 +0200

    app: add code to read/write XCF tile data as big-endian
    It was always supposed to be like that, but simply forgotten.
    Fortunately, big-endian machines are almost extinct...
    
    The new code is triggered with XCF version >= 12, but we will start
    using that only after code review.

--- app/xcf/xcf-load.c
+++ app/xcf/xcf-load.c
@@ -2005,7 +2005,17 @@ xcf_load_tile (XcfInfo       *info,
   gint    tile_size = bpp * tile_rect->width * tile_rect->height;
   guchar *tile_data = g_alloca (tile_size);
 
-  xcf_read_int8 (info, tile_data, tile_size);
+  if (info->file_version <= 11)
+    {
+      xcf_read_int8 (info, tile_data, tile_size);
+    }
+  else
+    {
+      gint n_components = babl_format_get_n_components (format);
+
+      xcf_read_component (info, bpp / n_components, tile_data,
+                          tile_size / n_components);
+    }
 
   gegl_buffer_set (buffer, tile_rect, 0, format, tile_data,
                    GEGL_AUTO_ROWSTRIDE);
@@ -2142,6 +2152,14 @@ xcf_load_tile_rle (XcfInfo       *info,
         }
     }
 
+  if (info->file_version >= 12)
+    {
+      gint n_components = babl_format_get_n_components (format);
+
+      xcf_read_from_be (bpp / n_components, tile_data,
+                        tile_size / n_components);
+    }
+
   gegl_buffer_set (buffer, tile_rect, 0, format, tile_data,
                    GEGL_AUTO_ROWSTRIDE);
 
@@ -2232,10 +2250,19 @@ xcf_load_tile_zlib (XcfInfo       *info,
         }
     }
 
+  if (info->file_version >= 12)
+    {
+      gint n_components = babl_format_get_n_components (format);
+
+      xcf_read_from_be (bpp / n_components, tile_data,
+                        tile_size / n_components);
+    }
+
   gegl_buffer_set (buffer, tile_rect, 0, format, tile_data,
                    GEGL_AUTO_ROWSTRIDE);
 
   inflateEnd (&strm);
+
   return TRUE;
 }
 
