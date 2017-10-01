b494a1f2aa9c5a236d1acc6c34ef067b2d5c4eec
Michael Natterer <mitch@gimp.org>
2017-10-01 17:27:04 +0200

    app: XCF endian swapping was still not correct
    The number of pixels is "tile_size / bpp", not "tile_size / n_components".

--- app/xcf/xcf-save.c
+++ app/xcf/xcf-save.c
@@ -1653,7 +1653,7 @@ xcf_save_tile_rle (XcfInfo        *info,
       gint n_components = babl_format_get_n_components (format);
 
       xcf_write_to_be (bpp / n_components, tile_data,
-                       tile_size / n_components);
+                       tile_size / bpp);
     }
 
   for (i = 0; i < bpp; i++)
@@ -1789,7 +1789,7 @@ xcf_save_tile_zlib (XcfInfo        *info,
       gint n_components = babl_format_get_n_components (format);
 
       xcf_write_to_be (bpp / n_components, tile_data,
-                       tile_size / n_components);
+                       tile_size / bpp);
     }
 
   /* allocate deflate state */
